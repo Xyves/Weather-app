3b15428977142ac5061b9e0d97974d903502a698
Xyves <xodrixx@protonmail.com>
2024-01-07 22:31:43 +0100

    Change background using giphy API image

--- src/index.js
+++ src/index.js
@@ -1,26 +1,51 @@
 window.addEventListener("DOMContentLoaded", () => {
-  checkCurrentLocation();
+  renderLocation();
 });
 // import "./styles.scss";
+// Retrieves and renders the images data from the pexels api
+function RenderImages() {}
+const apikey = "563492ad6f917000010000019b983f3b62fe43daa92e746d4553dd35";
+fetch("https://api.pexels.com/v1/search?query=green nature&per_page=40  ", {
+  method: "GET",
+  headers: {
+    Authorization: apikey,
+  },
+})
+  .then((response) => {
+    const json = response.json();
+    console.log(json + "hhh");
+    return json;
+  })
+  .then((response) => {
+    console.log(response);
+    console.log("1");
+    const number = Math.floor(Math.random() * 40);
+    setBackground(response, number);
+    return response;
+  });
 
-// fetch("https://api.pexels.com/v1/search?query=weather", {
-//   method: "GET",
-//   headers: {
-//     Authorization: ZPkwlkus8Y2GdSSIcOIv1g1c9m7hItHsfLKAZw7GbCztE2GyCt4BgJDB,
-//   },
-// }).then((response) => {
-//   return response.json();
-// });
+// Retrieves and renders the weather data of the current position
+function renderLocation() {
+  navigator.geolocation.getCurrentPosition((position) => {
+    if (navigator.geolocation) {
+      getCurrentLocation(position);
+
+      // renderWeather()
+    } else {
+      console.log("Geolocation is not supported by this browser.");
+    }
+  });
+}
 const temperatureButton = document.querySelector(".temperature");
 const searchButton = document.querySelector(".searchButton");
-
+const form = document.querySelector("form");
+// Renders the weather data of the searched city
 searchButton.addEventListener("click", (e) => {
   e.preventDefault();
   const searchInput = document.querySelector("#searchInput").value;
   changeCityApi(searchInput);
-  // console.log(location + "hey");
-  // changeCityTitle("", location);
 });
+
 temperatureButton.addEventListener("click", () => {
   console.log("wow");
   const tempSpan = document.querySelector(".temperature span");
@@ -31,18 +56,8 @@ temperatureButton.addEventListener("click", () => {
     tempSpan.value = "Celsius";
   }
 });
-function checkCurrentLocation() {
-  navigator.geolocation.getCurrentPosition((position) => {
-    if (navigator.geolocation) {
-      showCity(position);
-      console.log("log");
-    } else {
-      console.log("Geolocation is not supported by this browser.");
-    }
-  });
-}
 
-function showCity(position) {
+function getCurrentLocation(position) {
   const latitude = position.coords.latitude;
   const longitude = position.coords.longitude;
   const url = `https://geocode.maps.co/reverse?lat=${latitude}&lon=${longitude}&api_key=659926fa3f43f739566522ramf39f14`;
@@ -75,9 +90,9 @@ function changeCityTitle(address, addressWeather) {
     console.log("here" + JSON.stringify(addressWeather));
     let location = JSON.stringify(addressWeather);
     let l1 = JSON.parse(location);
-    let cityName = l1.name;
+    let cityName = l1.location.name;
     console.log(addressWeather + "Adress");
-    let countryName = l1.country;
+    let countryName = l1.location.country;
     let fullCityName = `Weather in ${cityName}, ${countryName}`;
     weatherH1.textContent = fullCityName;
   }
@@ -89,7 +104,7 @@ function changeCityApi(value) {
       return response.json();
     })
     .then((response) => {
-      const location = response.location;
+      const location = response;
       console.log(location);
       changeCityTitle("", location);
       return response;
@@ -97,4 +112,17 @@ function changeCityApi(value) {
     .catch((err) => {
       console.log(err);
     });
+}
+function setBackground(images, number) {
+  const container = document.querySelector(".container");
+  console.log("log function");
+  let stringData = JSON.stringify(images);
+  let parsedData = JSON.parse(stringData);
+  let imgSrc = parsedData.photos[number].src.original;
+  console.log(parsedData);
+  container.style.backgroundImage = `url(${imgSrc})`;
+  container.style.backgroundRepeat = "no-repeat";
+  container.style.backgroundPosition = "center";
+  container.style.backgroundSize = "cover";
+  container.style.imageRendering = "-webkit -optimize -contras";
 }
